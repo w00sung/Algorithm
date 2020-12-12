d6e91ebb94c45a3e82044427e69e471f17f70619
woosung2 <jeongws3240@gmail.com>
2020-12-12 21:14:26 +0900

    Z using self call

--- BOJ/1074_Z.py
+++ BOJ/1074_Z.py
@@ -5,29 +5,24 @@ read = sys.stdin.readline
 n,r,c = map(int,read().rstrip().split())
 # 2^n 의 행렬 count 먹이기
 
-def getCount(n,r,c):
-    cnt = 0
+def count(n,r,c):
+    if n == 0:
+        return 0
+    idx = 2**(n-1)
+    if r >= idx and c>= idx:
+        k = 3
+        r %= idx
+        c %= idx
+    elif r >= idx and c< idx:
+        k = 2
+        r %= idx
+    elif r < idx and c>= idx:
+        k = 1
+        c %= idx
+    else:
+        k = 0
+    
+    return idx*idx*k + count(n-1,r,c)
+print(count(n,r,c))
 
-    while n > 0:
-        idx = 2**(n-1)
-        if r >= idx and c>= idx:
-            k = 3
-            # 4로 나누는게 아니라, idx로 나눈다.
-            r %= idx
-            c %= idx
-        elif r >= idx and c< idx:
-            k = 2
-            r %= idx
-        elif r < idx and c>= idx:
-            k = 1
-            c %= idx
-        else:
-            k = 0
-        
-        cnt += idx*idx*k
-        n -= 1    
-
-    return cnt
-
-print(getCount(n,r,c))
    
