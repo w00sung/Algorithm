697285c8959efc73a1751c87ff11fc05b033a3b7
woosung2 <jeongws3240@gmail.com>
2020-12-12 21:32:25 +0900

    Z Fix if

--- BOJ/1074_Z.py
+++ BOJ/1074_Z.py
@@ -9,20 +9,14 @@ def count(n,r,c):
     if n == 0:
         return 0
     idx = 2**(n-1)
-    if r >= idx and c>= idx:
-        k = 3
-        r %= idx
-        c %= idx
-    elif r >= idx and c< idx:
-        k = 2
-        r %= idx
-    elif r < idx and c>= idx:
-        k = 1
-        c %= idx
-    else:
-        k = 0
+    k = idx ** 2
+    # A,B,C,D 0을 할지, 1을 할지 열로 검증
+    res = k if c >= idx else 0
+    # 2를 더할지, 더하지 않을지 행으로 **바로** 검증
+    res += 2*k if r >= idx else 0
     
-    return idx*idx*k + count(n-1,r,c)
+    # idx 로 나눠줘도, 앞에 행, 열은 영향받지 않는다.
+    return res + count(n-1,r%idx,c%idx)
 print(count(n,r,c))
 
    
