b5facdaa297d6b4ea5a14ff4544a6e2e96521ebd
woosung2 <jeongws3240@gmail.com>
2020-12-18 20:43:16 +0900

    divide conquer

--- BOJ/2630_making_paper.py
+++ BOJ/2630_making_paper.py
@@ -3,27 +3,52 @@ read = sys.stdin.readline
 
 n = int(read())
 arr = []
-cnt = [0 0]
+cnt = [0,0]
 for _ in range(n):
     arr.append(list(map(int,read().rstrip().split())))
 
 # arr 을 len(arr) // 2 를 하여 인덱스
-def cut(arr,n):
+
+## 길이에 맞춰서 for문 돌리기?
+# arr : 배열, x,y : 시작점 행,열
+def cut(arr,x,y,n):
+    global cnt
+    brk = False
+    theme = 0
+    theme = arr[x][y]
+
+    # 종료조건에 반드시 return !!!!!!!!!
+    if n==1:
+        cnt[theme] += 1
+        return
 
 
-    i = len(cut)
-    # 다 잘렸으면
-    if n == 0 :
-        if arr[0][0]:
-            cnt[0] += 1
-        else:
-            cnt[1] += 1
 
-    else:
-
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if arr[i][j] != theme:
+                # 초기값과 다른 값 등장시 짤!
+                brk = True
+                break
+        if brk:
+            break
+    
+    if brk:
         k = n // 2
-        cut(arr[0:k][0:k],k)
+        # y true 면 잘릴 일 없다.
+        # arr[0:k][0:k] 불가능함
+        ## 분할 --- 범위를 조절
+        # 1사분면
+        cut(arr,x,y,k)
+        # 2사분면
+        cut(arr,x,y+k,k)
+        # 3사분면
+        cut(arr,x+k,y,k)
+        # 4사분면
+        cut(arr,x+k,y+k,k)
+    else:
+        cnt[theme] += 1
 
-        cut(arr[k:n][k:n],k)
-
-        cut(arr[])
+cut(arr,0,0,n)
+print(cnt[0])
+print(cnt[1])
