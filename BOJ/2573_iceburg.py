8c4aa6dcd4bee9733dbddc2ee9b2fd6164641ca8
w00sung <jeongws3240@gmail.com>
2020-12-27 19:40:23 +0900

    iceburg - using DFS & queue and visitied for finding

--- BOJ/2573_iceburg.py
+++ BOJ/2573_iceburg.py
@@ -12,42 +12,53 @@ for _ in range(N):
 
 visited = [[False] * M for _ in range(N)]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
 
-
+# 초기에 큐에 넣어주기
+for i in range(1,N):
+    for j in range(1,M):
+        if iceburg[i][j] != 0:
+            xyqueue.append((i,j))
 
 
 # 숫자 줄이기
 def after_year(x,y):
     
-
+    # 빙산은 못 세게 수 있는 장치
+    visited[x][y] = True
+    
     cnt_0 = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    for dx,dy in (1,0),(0,1),(-1,0),(0,-1):
+        nx = x + dx
+        ny = y + dy
 
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
-            continue
+        # 벽을 만날 일이 없구나
+        # if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        #     continue
 
-        if iceburg[nx][ny] == 0:
+        if visited[nx][ny] == False and iceburg[nx][ny] == 0:
             cnt_0 += 1
             
     # 빙산 높이 줄이기 
     # -- 같은 시간의 바로 옆 빙산도 널 0으로 본다.
     iceburg[x][y] -= cnt_0
 
+
+    # visited --- 0 셀 때 활용하기
+
+
     # 0 되면 끝내기 
-    # -- G이면 0으로 바꿔주자.
+    # -- 보완 -- G이면 0으로 바꿔주자.
     if iceburg[x][y] <= 0:
-        iceburg[x][y] = "G"
+        iceburg[x][y] = 0
 
         return
 
-    # # 살아있으면 다시 넣어주기
-    # elif iceburg[x][y] >= 1:
-    #     xyqueue.append((x,y))
-    #     return
+    # 살아있으면 다시 넣어주기
+    elif iceburg[x][y] > 0:
+        xyqueue.append((x,y))
+        return
 
 
 
@@ -62,16 +73,17 @@ def count_iceburg(x,y):
     # else:
     visited[x][y] = True
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    for dx,dy in (1,0),(0,1),(-1,0),(0,-1):
+        nx = x + dx
+        ny = y + dy
 
         # 벽에 부딪히면 안감
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
-            continue
+        # -- 벽에 부딪힐 일이 없음
+        # if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        #     continue
         
         # 빙산이 있고, 방문안했으면, 들어가기
-        if iceburg[nx][ny] != "G" and iceburg[nx][ny] >= 1 and visited[nx][ny] == False:
+        if iceburg[nx][ny] >= 1 and visited[nx][ny] == False:
             count_iceburg(nx,ny)
 
     #조각 다 세고 돌아오면 True
@@ -81,17 +93,17 @@ def count_iceburg(x,y):
 year = 0
 piece = 0
 while True:
-
+    
     # 빙산 세기
-    for i in range(1,N):
-        for j in range(1,M):
-            # 이전 빙하들 0으로 업데이트
-            if iceburg[i][j] == "G":
-                iceburg[i][j] = 0
+    # -- 더 멋진 방법 없나 ? --
+    # for i in range(1,N):
+    #     for j in range(1,M):
+    #         # 이전 빙하들 0으로 업데이트
+    #         if iceburg[i][j] == "G":
+    #             iceburg[i][j] = 0
 
-            if iceburg[i][j] != 0:
-                xyqueue.append((i,j))
-
+    #         if iceburg[i][j] != 0:
+    #             xyqueue.append((i,j))
 
     # 조각 2개되기 전에 0 되면,
     if not xyqueue:
@@ -111,15 +123,17 @@ while True:
     # 두덩어리 "이상"!!!!
     if piece >= 2:
         break
-    
+    cnt = 0
+    len_xy = len(xyqueue)
+
+    visited = [[False] * M for _ in range(N)]
     # 하루 녹는값 update
-    while xyqueue :
+    while cnt < len_xy :
 
         x,y = xyqueue.popleft()
+        cnt += 1
         after_year(x,y)
 
-    # 숫자 update 후에 조각 세주기
-    
     year += 1
     
 print(year)
