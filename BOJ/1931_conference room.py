3c1968fbe465d079830ccb13734f3122ae750482
w00sung <jeongws3240@gmail.com>
2021-01-05 21:48:16 +0900

    CHECK ERROR and FIX IT

--- BOJ/1931_conference room.py
+++ BOJ/1931_conference room.py
@@ -1,34 +1,43 @@
 import sys
 read = sys.stdin.readline
 
-N = int(read())
-conferences =[]
+def solve():
+    N = int(read())
+    conferences =[]
+    for _ in range(N):
+        strt, end = map(int,read().rstrip().split())
+        conferences.append((strt,end))
 
-for _ in range(N):
-    strt, end = map(int,read().rstrip().split())
-    conferences.append((strt,end))
+    conferences = sorted(conferences, key = lambda x : x[0])
 
-conferences = sorted(conferences, key = lambda x : x[0])
+    ## 일찍 시작하는 것이 만능은 아니다.
+    # 영역 안에 많은 개수가 들어오는게 더 좋다.
+    # (2,5) 보다 (3,3)(3,4)(4,5) 가 좋다.
 
-## 일찍 시작하는 것이 만능은 아니다.
-# 영역 안에 많은 개수가 들어오는게 더 좋다.
-# (2,5) 보다 (3,3)(3,4)(4,5) 가 좋다.
+    # 첫 친구의 끝 시간 
+    criteria = float('inf')
+    check_strt = 0
+    cnt = 1
+    for strt,end in conferences:
 
-# 첫 친구의 끝 시간 
-criteria = float('inf')
-cnt = 1
-for strt,end in conferences:
+        # 기준보다 빨리 시작하고, 빨리 끝나는 친구 있으면,\
+        if strt < criteria and end < criteria:
+            
+            # 시작시간, 종료시간이 마지막하고 같은 놈이 제일 마지막에 위치하면, 걔는 세줘야됨
+            if strt == check_strt and strt == end:
+                cnt += 1
 
-    # 기준보다 빨리 시작하고, 빨리 끝나는 친구 있으면,
-    if strt < criteria and end < criteria:
+            # 기준을 바꿔준다. -- 걔가 더 좋은 놈
+            criteria = end
+            check_strt = strt
+        # 시작 끝 같은 놈도 있음!
+        # 앞 영역 시간 끝 ! 뒷 영역 시간 시작!
+        elif strt >= criteria:
+            cnt += 1
+            criteria = end
+            check_strt = strt
 
-        #기준을 바꿔준다.
-        criteria = end
 
-    # 시작 끝 같은 놈도 있음!
-    # 앞 영역 시간 끝 ! 뒷 영역 시간 시작!
-    elif strt >= criteria:
-        cnt += 1
-        criteria = end
+    print(cnt)
 
-print(cnt)
+solve()
